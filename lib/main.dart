a75716cef1b4f68e671fcda015c987f43337969b
Ahmed Jawad <53703523+AhmedJwad@users.noreply.github.com>
2022-08-21 22:37:20 -0800

    login
    Add login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:healthcare/screen/login_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -6,17 +7,12 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {    
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'HealthCare App',
+      home: loginScreen(),
+      
     );
   }
 }
---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,53 @@
+
+
+import 'package:flutter/material.dart';
+
+class loginScreen extends StatefulWidget {
+  const loginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<loginScreen> createState() => _loginScreenState();
+}
+
+class _loginScreenState extends State<loginScreen> {
+  String _email = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Column(
+        mainAxisAlignment: MainAxisAlignment.center,       
+        children: <Widget>[
+         _showlogo(),
+         _showemail(),
+        ],
+      )
+      
+
+    );
+  }
+  
+ Widget _showlogo() {
+
+  return Image(image: AssetImage('assets/logo.jpg'), width: 300,);
+ }
+ 
+ Widget _showemail() {
+  return Container(
+    padding: EdgeInsets.all(20),
+    child: TextField(
+      autocorrect: true,
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'please inserrt your email',
+        labelText: 'email',
+        suffixIcon: Icon(Icons.email),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),      
+      ),
+      onChanged: (value){
+        _email = value;
+        print(_email);
+      },
+    ),
+  );
+ }
+}
